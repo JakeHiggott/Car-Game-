274e8fa9fcc61eaaa91233acef2347adb0eaf572
Jake Higgot <jakehiggot@interapts-macbook-pro-101.local>
2018-02-08 13:27:00 -0500

    Initial Commit

---
+++ Car Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Car Project
+//
+//  Created by Jake Higgot on 2/8/18.
+//  Copyright © 2018 Jake Higgott. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
